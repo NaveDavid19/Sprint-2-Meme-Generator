b05c9bd48bc0541d6b6e2b778b8002f84eae41fa
NaveDavid19 <navedavid16@gmail.com>
2023-12-03 09:54:16 +0200

    Moved download&upload controller to gallery controller

--- js/controllers/download-controller.js
+++
@@ -1,6 +0,0 @@
-'use strict'
-// function DownloadImg
-function downloadImg(elLink) {
-    const imgContent = gElCanvas.toDataURL('image/jpeg')
-    elLink.href = imgContent
-}
--- js/controllers/gallery-controller.js
+++ js/controllers/gallery-controller.js
@@ -28,3 +28,35 @@ function onSelectImg(elImg) {
 
     renderMeme()
 }
+
+// function DownloadImg
+function downloadImg(elLink) {
+    const imgContent = gElCanvas.toDataURL('image/jpeg')
+    elLink.href = imgContent
+}
+
+function onImgInput(ev) {
+    loadImageFromInput(ev, renderImg)
+}
+
+function loadImageFromInput(ev, onImageReady) {
+    const reader = new FileReader()
+    reader.onload = function (event) {
+        let img = new Image()
+        img.src = event.target.result
+        img.onload = () => onImageReady(img)
+    }
+    reader.readAsDataURL(ev.target.files[0])
+}
+
+function renderImg(img) {
+    let elGallery = document.querySelector('.gallery')
+    var elTitle = document.querySelector('.title-gallery')
+    elTitle.classList.add('hide')
+    elGallery.classList.add('hide')
+    let elMeme = document.querySelector('.memes')
+    elMeme.classList.remove('hide')
+    resetMeme()
+    renderMeme()
+    gCtx.drawImage(img, 0, 0, gElCanvas.width, gElCanvas.height)
+}
